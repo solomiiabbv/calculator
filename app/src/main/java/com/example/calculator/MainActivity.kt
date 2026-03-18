54d4c6567db0734872a8795e57530ed6b4d37c7d
solomiiabbv <solomiya.babijiv@kpk-lp.com.ua>
2026-03-19 01:22:52 +0200

    одано обробку кнопки 1

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,21 +1,29 @@
 package com.example.calculator
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    // TextView для відображення результату
+    private lateinit var tvResult: TextView
+
+    // Змінна для збереження введеного виразу
+    private var currentInput = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Підключення TextView
+        tvResult = findViewById(R.id.tvResult)
+        // Обробка кнопки 1
+        findViewById<Button>(R.id.btn1).setOnClickListener {
+            currentInput += "1"
+            tvResult.text = currentInput
         }
     }
+
 }
-//І
